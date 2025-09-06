255802ad98cdb9a4b60dcf01488b72ca376ed449
Ellie Wright <ellie952@revature.net>
2025-09-06 16:28:49 -0500

    Create first endpoint

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const { logger } = require("./logger/logger.js");
+const users = require("./data/users.json");
 
 const server = express();
 
 const PORT = 3000;
 
+const HttpStatusCodes = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    METHOD_NOT_ALLOWED: 405,
+    INTERNAL_SERVER_ERROR: 500
+}
+
 server.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.status(HttpStatusCodes.OK);
+    res.json({
+        message: "Users retrieved.",
+        data: users
+    });
 });
 
 server.listen(PORT, () => {
